dae0ebb29407563a1ed4ffd9df1182a10ae91d7a
cherus12 <22rmk22@mail.ru>
2025-02-19 15:51:10 +0300

    server.js

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("", "http://localhost:1337"),
+  url: env("PUBLIC_URL", "https://car-backend-finish.onrender.com"),
   app: {
     keys: env.array("APP_KEYS"),
   },
@@ -9,3 +9,15 @@ module.exports = ({ env }) => ({
     populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
 });
+
+// module.exports = ({ env }) => ({
+//   host: env("HOST", "0.0.0.0"),
+//   port: env.int("PORT", 1337),
+//   url: env("", "http://localhost:1337"),
+//   app: {
+//     keys: env.array("APP_KEYS"),
+//   },
+//   webhooks: {
+//     populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+//   },
+// });
